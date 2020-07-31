8ca6ca901256227630dd82be60b5922697e24e3a
Erik Scow <erikscow@gmail.com>
2020-07-31 01:49:57 -0700

    Added seed file for intermediary table

---
+++ data/seeds/003-recipe-ingredients-intermediary.js
@@ -0,0 +1,40 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('recipe-ingredients-intermediary').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('recipe-ingredients-intermediary').insert([
+        {
+          recipe_id: 1,
+          ingredient_id:1,
+          ingredient_quantity: 2
+        },
+        {
+          recipe_id: 1,
+          ingredient_id:2,
+          ingredient_quantity: 2
+        },
+        {
+          recipe_id: 2,
+          ingredient_id:3,
+          ingredient_quantity: 2.3
+        },
+        {
+          recipe_id: 2,
+          ingredient_id:4,
+          ingredient_quantity: 2
+        },
+        {
+          recipe_id: 3,
+          ingredient_id:5,
+          ingredient_quantity: 2.5
+        },
+        {
+          recipe_id: 3,
+          ingredient_id:6,
+          ingredient_quantity: 2.5
+        }
+      ]);
+    });
+};
